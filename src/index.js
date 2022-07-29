4296970f4abc35832fb38d8a4c37695f129f17ea
AlforovaNata <ndalferova@gmail.com>
2022-07-29 16:22:33 +0200

    Add description/conversion

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-//Feature #1
 let weekDay = document.querySelector("#currentDay");
 let now = new Date();
 let dayToday = now.getDay();
-let year = now.getFullYear();
 let hours = now.getHours();
-let date = now.getDate();
 let minutes = now.getMinutes();
 let days = [
   "Sunday",
@@ -15,22 +12,6 @@ let days = [
   "Friday",
   "Saturday",
 ];
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-
-let month = months[now.getMonth()];
 
 if (hours < 10) {
   hours = `0${hours}`;
@@ -39,9 +20,8 @@ if (hours < 10) {
 if (minutes < 10) {
   minutes = `0${minutes}`;
 }
-weekDay.innerHTML = `${days[dayToday]} <br/> ${date} ${month} ${year} <br/> ${hours}:${minutes}`;
+weekDay.innerHTML = `Last updated</br>${days[dayToday]} ${hours}:${minutes}`;
 
-//Feature #2
 function showCity(event) {
   event.preventDefault();
   let inputCity = document.querySelector("#input-city");
@@ -57,34 +37,51 @@ function showCity(event) {
 let cityWeather = document.querySelector("#city-weather");
 cityWeather.addEventListener("submit", showCity);
 
-/*Feature #3
-function showTemperature() {
- let showTemp = document.querySelector("#temperature");
- showTemp.innerHTML = 20;
+function showFahrenheit(event) {
+  event.preventDefault();
+  celsiusTemp.classList.remove("active");
+  fahrenheitTemp.classList.add("active");
+  let temperatureElement = document.querySelector("#temperature");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+function showCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-function showFahrenheit() {
-let showFahren = document.querySelector("#temperature");
- showFahren.innerHTML = 68;
-}
+let celsiusTemperature = null;
 
-let showTemp = document.querySelector("#celsius");
-showTemp.addEventListener("click", showTemperature);
+let fahrenheitTemp = document.querySelector("#fahrenheit");
+fahrenheitTemp.addEventListener("click", showFahrenheit);
 
-let showFahren = document.querySelector("#fahrenheit");
-showFahren.addEventListener("click", showFahrenheit);*/
+let celsiusTemp = document.querySelector("#celsius");
+celsiusTemp.addEventListener("click", showCelsius);
+
 function displayWeather(response) {
   let h1 = document.querySelector("#city");
   h1.innerHTML = response.data.name;
   let temperature = Math.round(response.data.main.temp);
   let todayTemp = document.querySelector("#temperature");
   todayTemp.innerHTML = `${temperature}`;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
   let realFeel = document.querySelector("#feels");
   realFeel.innerHTML = Math.round(response.data.main.feels_like);
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = response.data.main.humidity;
   let windSpeed = document.querySelector("#wind");
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  let pressure = document.querySelector("#pressure");
+  pressure.innerHTML = response.data.main.pressure;
+  let iconElement = document.querySelector("#weather-icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+  celsiusTemperature = response.data.main.temp;
 }
 
 function searchCity(city) {
@@ -94,8 +91,6 @@ function searchCity(city) {
 
   axios.get(apiUrl).then(displayWeather);
 }
-
-searchCity("Lviv");
 
 function submitCity(event) {
   event.preventDefault();
@@ -126,3 +121,5 @@ searchForm.addEventListener("submit", submitCity);
 
 let currentButton = document.querySelector("#current-location-button");
 currentButton.addEventListener("click", getPosition);
+
+searchCity("Lviv");
